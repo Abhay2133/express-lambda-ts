5df2dfba7fdcc6a12555771fa21ac3bea7643c45
abhay2133 <abhaybishthestudent@gmail.com>
2025-04-27 17:58:52 +0530

    testing gh-actions

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.status(200).json({message: "Server running ;)"});
+  res.status(200).json({message: "Server running :]"});
 });
 
 export default app;
